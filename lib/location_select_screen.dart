60bde0f40d24d72253998bfad24ff46b55ee71f1
vektemok <lunnoa15@gmail.com>
2023-06-14 19:44:50 +0600

    first commit

---
+++ lib/location_select_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class LocationSelectScreen extends StatefulWidget {
+  const LocationSelectScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LocationSelectScreen> createState() => _LocationSelectScreenState();
+}
+
+class _LocationSelectScreenState extends State<LocationSelectScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
