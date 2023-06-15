23905180e8a3b785c6de1dafada610316a201729
vektemok <lunnoa15@gmail.com>
2023-06-15 09:21:38 +0600

    first commit

--- lib/src/config/themes/white_theme.dart
+++ lib/src/config/themes/white_theme.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 
 final ThemeData whiteTheme = ThemeData(
+  backgroundColor: Colors.white,
   appBarTheme:  const AppBarTheme(
     toolbarHeight: 100,
     backgroundColor: Colors.white,
--- lib/src/presentation/widgets/screen_switcher_button.dart
+++ lib/src/presentation/widgets/screen_switcher_button.dart
@@ -1,41 +1,55 @@
-import 'package:flutter/material.dart';
 
 
-class ScreenSwitcherButton extends StatelessWidget {
-  final String title;
 
 
-   ScreenSwitcherButton({Key? key, required this.title})
-      : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
 
-      child:  Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Container(
-width: 260,
-          height: 70,
 
-          decoration: BoxDecoration(
-color: Colors.white
-          ),
-          child:
-          Center(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Align(
-                  alignment: Alignment.center,
-                  child: Text(title),
-                ),
-                Icon(Icons.arrow_forward_rounded)
-              ],
-            ),
-          ) ,
-        ),
-      ),
-    );
-  }
-}
+
+// NO USING
+
+
+
+
+
+// import 'package:flutter/material.dart';
+//
+//
+// class ScreenSwitcherButton extends StatelessWidget {
+//   final String title;
+//
+//
+//    ScreenSwitcherButton({Key? key, required this.title})
+//       : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return InkWell(
+//
+//       child:  Padding(
+//         padding: const EdgeInsets.all(8.0),
+//         child: Container(
+// width: 260,
+//           height: 70,
+//
+//           decoration: BoxDecoration(
+// color: Colors.white
+//           ),
+//           child:
+//           Center(
+//             child: Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//               children: [
+//                 Align(
+//                   alignment: Alignment.center,
+//                   child: Text(title),
+//                 ),
+//                 Icon(Icons.arrow_forward_rounded)
+//               ],
+//             ),
+//           ) ,
+//         ),
+//       ),
+//     );
+//   }
+// }
