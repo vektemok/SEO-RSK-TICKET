319f99db8d5c6e97d9a7c31bcf04cbf7e3a9cc0b
vektemok <lunnoa15@gmail.com>
2023-06-15 17:02:45 +0600

    add nav bar icons svg

--- lib/src/presentation/view/screens/main_screen.dart
+++ lib/src/presentation/view/screens/main_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:united102/src/presentation/view/screens/notification_screen.dart';
+import 'package:united102/src/presentation/view/screens/setting_screen.dart';
+import 'package:united102/src/presentation/view/screens/ticket_screen.dart';
 
 import 'categories_screen.dart';
 
@@ -11,10 +14,24 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  List<Widget> _screens = [CategoriesScreen()];
+  List<Widget> _screens = [CategoriesScreen(),
+    TicketScreen(),
+    NotificationScreen(),
+    SettingScreen()
+
+
+
+
+  ];
 
   int _currentIndex = 0;
 
+
+  void onTap(int index){
+    setState(() {
+      _currentIndex = index;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,19 +49,36 @@ class _MainScreenState extends State<MainScreen> {
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
+        onTap: onTap,
         showSelectedLabels: false,
         showUnselectedLabels: false,
         items: [
+          //1
+          BottomNavigationBarItem(
+              icon: SvgPicture.asset('assets/nav_bar_icons/home_na.svg'),
+              activeIcon: SvgPicture.asset('assets/nav_bar_icons/home.svg'),
+              label: ''),
+          //2
+          BottomNavigationBarItem(
+              icon:
+                  SvgPicture.asset('assets/nav_bar_icons/ticket_na.svg'),
+              activeIcon:
+                  SvgPicture.asset('assets/nav_bar_icons/ticket.svg'),
+              label: ''),
+          //3
           BottomNavigationBarItem(
               icon: SvgPicture.asset('assets/nav_bar_icons/home.svg'),
               activeIcon: SvgPicture.asset('assets/nav_bar_icons/home.svg'),
               label: ''),
+          //4
           BottomNavigationBarItem(
               icon:
                   SvgPicture.asset('assets/nav_bar_icons/notification_na.svg'),
               activeIcon:
                   SvgPicture.asset('assets/nav_bar_icons/notification.svg'),
               label: ''),
+
+
         ],
       ),
     );
