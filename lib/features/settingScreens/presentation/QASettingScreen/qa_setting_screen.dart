8f92e4a4c14d6ffaf96fce05dd08e6e5e81600a1
vektemok <lunnoa15@gmail.com>
2023-06-19 05:46:58 +0600

    first commit

--- lib/features/settingScreens/presentation/QASettingScreen/qa_setting_screen.dart
+++ lib/features/settingScreens/presentation/QASettingScreen/qa_setting_screen.dart
@@ -10,7 +10,7 @@ class Item {
   });
 
 
-  List<String> headerT
+
   String expandedValue;
   String headerValue;
   bool isExpanded;
@@ -29,32 +29,33 @@ class _QASettingScreenState extends State<QASettingScreen> {
 
     const List<String> _data = [];
 
-    return Scaffold(
-      body: SafeArea(
-        child: ListView.builder(
-            itemCount: 7,
-            itemBuilder: (context, index){
-          return ExpansionPanelList(
-            children:_data.map<ExpansionPanel>((Item item) {
-              return ExpansionPanel(
-                headerBuilder: (BuildContext context, bool isExpanded) {
-                  return ListTile(
-                    title: Text(item.headerValue),
-                  );
-                },
-                body: ListTile(
-                    title: Text(item.expandedValue),
-                    subtitle:
-                    const Text('To delete this panel, tap the trash can icon'),
-                    trailing: const Icon(Icons.delete),
-                  ),
-                isExpanded: item.isExpanded,
-              );
-            }).toList(),
-          );
-        }),
-      ),
-    )
+    // return Scaffold(
+    //   body: SafeArea(
+    //     child: ListView.builder(
+    //         itemCount: 7,
+    //         itemBuilder: (context, index){
+    //       return ExpansionPanelList(
+    //         children:_data.map<ExpansionPanel>((Item item) {
+    //           return ExpansionPanel(
+    //             headerBuilder: (BuildContext context, bool isExpanded) {
+    //               return ListTile(
+    //                 title: Text(item.headerValue),
+    //               );
+    //             },
+    //             body: ListTile(
+    //                 title: Text(item.expandedValue),
+    //                 subtitle:
+    //                 const Text('To delete this panel, tap the trash can icon'),
+    //                 trailing: const Icon(Icons.delete),
+    //               ),
+    //             isExpanded: item.isExpanded,
+    //           );
+    //         }).toList(),
+    //       );
+    //     }),
+    //   ),
+    // )
+    return Scaffold();
   }
 }
 
