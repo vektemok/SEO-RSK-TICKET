0cee0f4e124304c66378402663d24f205205f064
Ghosty-git <83651636+Ghosty-git@users.noreply.github.com>
2023-06-20 15:16:41 +0600

    add messScreen

--- lib/features/notificationScreens/presentation/NotificationScreen/notification_screen.dart
+++ lib/features/notificationScreens/presentation/NotificationScreen/notification_screen.dart
@@ -1,15 +1,142 @@
 import 'package:flutter/material.dart';
-
-
-/*
-
- */
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:united102/iternal/helpers/color_helper.dart';
 
 class NotificationScreen extends StatelessWidget {
   const NotificationScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: SvgPicture.asset("assets/appbar_rsk.svg"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10.w),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 77.h,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: 10.w,
+                ),
+                Text(
+                  "Новые",
+                  style: TextStyle(fontSize: 18.sp, fontFamily: "Montserrat"),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 5.h,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 1,
+                itemBuilder: (context, index) {
+                  return Container(
+                    decoration: BoxDecoration(
+                        color: ColorHelper.blue07,
+                        borderRadius: BorderRadius.circular(8.r)),
+                    padding: EdgeInsets.symmetric(horizontal: 19.w),
+                    width: 410.w,
+                    height: 54.h,
+                    child: Row(
+                      children: [
+                        const Icon(
+                          Icons.access_alarm_sharp,
+                          size: 40,
+                          color: Colors.blue,
+                        ),
+                        SizedBox(
+                          width: 43.w,
+                        ),
+                        Text(
+                          "Ваша очередь началась",
+                          textAlign: TextAlign.center,
+                          style:
+                              TextStyle(color: Colors.white, fontSize: 16.sp),
+                        ),
+                        SizedBox(
+                          width: 65.w,
+                        ),
+                        Text(
+                          "08:42",
+                          textAlign: TextAlign.center,
+                          style:
+                              TextStyle(color: Colors.white, fontSize: 16.sp),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+            SizedBox(height: 40.h),
+             Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: 10.w,
+                ),
+                Text(
+                  "Прочитанные",
+                  style: TextStyle(fontSize: 18.sp, fontFamily: "Montserrat"),
+                ),
+              ],
+            ),
+             Expanded(
+              child: ListView.builder(
+                itemCount: 1,
+                itemBuilder: (context, index) {
+                  return Container(
+                    
+                    decoration: BoxDecoration(
+                    
+                        borderRadius: BorderRadius.circular(8.r),
+                        border: Border.all(color: ColorHelper.grey1)),
+                    padding: EdgeInsets.symmetric(horizontal: 19.w),
+                    width: 410.w,
+                    height: 54.h,
+                    child: Row(
+                      children: [
+                        const Icon(
+                          Icons.access_alarm_sharp,
+                          size: 40,
+                          color: Colors.blue,
+                        ),
+                        SizedBox(
+                          width: 43.w,
+                        ),
+                        Text(
+                          "Ваша очередь началась",
+                          textAlign: TextAlign.center,
+                          style:
+                              TextStyle(color: ColorHelper.grey1, fontSize: 16.sp),
+                        ),
+                        SizedBox(
+                          width: 63.w,
+                        ),
+                        Text(
+                          "08:42",
+                          textAlign: TextAlign.center,
+                          style:
+                              TextStyle(color: ColorHelper.grey1, fontSize: 16.sp),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/features/settingScreens/presentation/QASettingScreen/qa_setting_screen.dart
+++ lib/features/settingScreens/presentation/QASettingScreen/qa_setting_screen.dart
@@ -2,61 +2,61 @@ import 'package:flutter/material.dart';
 
 
 
-class Item {
-  Item({
-    required this.expandedValue,
-    required this.headerValue,
-    this.isExpanded = false,
-  });
+// class Item {
+//   Item({
+//     required this.expandedValue,
+//     required this.headerValue,
+//     this.isExpanded = false,
+//   });
 
 
-  List<String> headerT
-  String expandedValue;
-  String headerValue;
-  bool isExpanded;
-}
+//   List<String> headerT;
+//   String expandedValue;
+//   String headerValue;
+//   bool isExpanded;
+// }
 
-class QASettingScreen extends StatefulWidget {
-  const QASettingScreen({Key? key}) : super(key: key);
+// class QASettingScreen extends StatefulWidget {
+//   const QASettingScreen({Key? key}) : super(key: key);
 
-  @override
-  State<QASettingScreen> createState() => _QASettingScreenState();
-}
+//   @override
+//   State<QASettingScreen> createState() => _QASettingScreenState();
+// }
 
-class _QASettingScreenState extends State<QASettingScreen> {
-  @override
-  Widget build(BuildContext context) {
+// class _QASettingScreenState extends State<QASettingScreen> {
+//   @override
+//   Widget build(BuildContext context) {
 
-    const List<String> _data = [];
+//     const List<String> _data = [];
 
-    return Scaffold(
-      body: SafeArea(
-        child: ListView.builder(
-            itemCount: 7,
-            itemBuilder: (context, index){
-          return ExpansionPanelList(
-            children:_data.map<ExpansionPanel>((Item item) {
-              return ExpansionPanel(
-                headerBuilder: (BuildContext context, bool isExpanded) {
-                  return ListTile(
-                    title: Text(item.headerValue),
-                  );
-                },
-                body: ListTile(
-                    title: Text(item.expandedValue),
-                    subtitle:
-                    const Text('To delete this panel, tap the trash can icon'),
-                    trailing: const Icon(Icons.delete),
-                  ),
-                isExpanded: item.isExpanded,
-              );
-            }).toList(),
-          );
-        }),
-      ),
-    )
-  }
-}
+//     return Scaffold(
+//       body: SafeArea(
+//         child: ListView.builder(
+//             itemCount: 7,
+//             itemBuilder: (context, index){
+//           return ExpansionPanelList(
+//             children:_data.map<ExpansionPanel>((Item item) {
+//               return ExpansionPanel(
+//                 headerBuilder: (BuildContext context, bool isExpanded) {
+//                   return ListTile(
+//                     title: Text(item.headerValue),
+//                   );
+//                 },
+//                 body: ListTile(
+//                     title: Text(item.expandedValue),
+//                     subtitle:
+//                     const Text('To delete this panel, tap the trash can icon'),
+//                     trailing: const Icon(Icons.delete),
+//                   ),
+//                 isExpanded: item.isExpanded,
+//               );
+//             }).toList(),
+//           );
+//         }),
+//       ),
+//     )
+//   }
+// }
 
 
 
