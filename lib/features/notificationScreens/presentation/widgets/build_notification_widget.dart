d3fb96efe91a9bb58cf09e177153d58e3b0b6c00
vektemok <lunnoa15@gmail.com>
2023-06-24 16:56:45 +0600

    commit

--- lib/features/notificationScreens/presentation/widgets/build_notification_widget.dart
+++ lib/features/notificationScreens/presentation/widgets/build_notification_widget.dart
@@ -23,19 +23,20 @@ class _BuildNotificationCardState extends State<BuildNotificationCard> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.fromLTRB(0, 5 , 0,  5),
+      padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
       child: Container(
         padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
         decoration: BoxDecoration(
-            // boxShadow: widget.boxShadow == true? Box,
-          color: widget.isNew == true? const Color(0xb23993c3): Colors.white,
-            boxShadow: widget.isNew == true? [
-           const   BoxShadow(
-                color:  Color(0x3f000000),
-                offset:  Offset(0, 2),
-                blurRadius: 2
+          // boxShadow: widget.boxShadow == true? Box,
+            color: widget.isNew == true ? const Color(0xb23993c3) : Colors
+                .white,
+            boxShadow: widget.isNew == true ? [
+              const BoxShadow(
+                  color: Color(0x3f000000),
+                  offset: Offset(0, 2),
+                  blurRadius: 2
               )
-            ]: null,
+            ] : null,
             borderRadius: BorderRadius.circular(8),
             border: Border.all(
                 width: 0.5, color: const Color.fromRGBO(166, 166, 166, 1))),
@@ -57,15 +58,15 @@ class _BuildNotificationCardState extends State<BuildNotificationCard> {
                 widget.item.title,
                 style: widget.isNew == true
                     ? GoogleFonts.montserrat(
-                        textStyle: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w400,
-                            fontSize: 16.sp))
+                    textStyle: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 16.sp))
                     : GoogleFonts.montserrat(
-                        textStyle: TextStyle(
-                            color: const Color.fromRGBO(116, 116, 116, 1),
-                            fontWeight: FontWeight.w400,
-                            fontSize: 16.sp)),
+                    textStyle: TextStyle(
+                        color: const Color.fromRGBO(116, 116, 116, 1),
+                        fontWeight: FontWeight.w400,
+                        fontSize: 16.sp)),
               ),
             ),
             const Spacer(),
