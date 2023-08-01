ac317cffcc39e7414b56ce102cb9a33546dfc907
vektemok <lunnoa15@gmail.com>
2023-08-01 19:47:16 +0600

    commit

--- lib/features/logic/bloc/booking_bloc/booking_bloc.dart
+++ lib/features/logic/bloc/booking_bloc/booking_bloc.dart
@@ -25,7 +25,7 @@ class BookingBloc extends Bloc<BookingEvent, BookingState> {
           date: event.date,
           queue: event.queue);
       final String toker =
-          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjkwODk2OTAyLCJpYXQiOjE2OTA4Nzg5MDIsImp0aSI6ImFjZjRmMzA1NTMzMTQ3ZWJhNmZkZWU3Y2MxYWYzNzFlIiwidXNlcl9pZCI6NTA1fQ.5drK0jnavE9m4j8VCelPpOWBvGpVB9NXQPW2IG2vL0k';
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjkwOTE1NTU5LCJpYXQiOjE2OTA4OTc1NTksImp0aSI6ImZmOGFiNzY5NzcyNzRjM2U5MjE1OWYzMDkwZTUzYjJlIiwidXNlcl9pZCI6NTA1fQ.GvneMK9m08ZNytlV3sQ9h1KMtgGDcA3rOKoaPSpojFs';
       try {
         final response = await http.post(
             Uri.parse('http://35.228.114.191/booking/'),
